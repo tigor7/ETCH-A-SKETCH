e6830307a06c0f7382a1c85e2474b77f31d7c061
Santiago Pujol <santiagopujol7@gmail.com>
2021-05-28 22:31:48 +0100

    grid of cells created

---
+++ index.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 256 ; i++){
+    const container = document.querySelector('.container')
+    let cell = document.createElement('div')
+    cell.classList.add('cell')
+    container.appendChild(cell)
+}

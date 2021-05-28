2fca935a7cd5b935d6322509b5e9451a8cf8219b
Santiago Pujol <santiagopujol7@gmail.com>
2021-05-28 22:53:55 +0100

    cells displays colors mouseover

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ for (let i = 0; i < 256 ; i++){
     const container = document.querySelector('.container')
     let cell = document.createElement('div')
     cell.classList.add('cell')
+    cell.addEventListener('mouseover', (e) => {
+        colorRed = Math.floor(Math.random() * 256)
+        colorGreen = Math.floor(Math.random() * 256)
+        colorBlue = Math.floor(Math.random() * 256)
+        cell.style.background = `rgb(${colorRed}, ${colorGreen}, ${colorBlue})`
+    })
     container.appendChild(cell)
 }
+

1c0280131b667f7ec79e2e105eace8d9643409a8
Santiago Pujol <santiagopujol7@gmail.com>
2021-05-29 17:48:33 +0100

    mvp finished

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 createGrid()
-function createGrid() {
-    for (let i = 0; i < 256 ; i++){
+function createGrid(size = 16) {
+    for (let i = 0; i < size * size ; i++){
         const container = document.querySelector('.container')
+        container.setAttribute('style', `--size: ${size}`);
         let cell = document.createElement('div')
         cell.classList.add('cell')
         cell.addEventListener('mouseover', (e) => {
@@ -13,8 +14,23 @@ function createGrid() {
         container.appendChild(cell)
     }
 }
-const btn = document.querySelector('button')
-btn.addEventListener('click', () => {
+const clearBtn = document.querySelector('#clear-btn')
+clearBtn.addEventListener('click', () => {
+    const container = document.querySelector('.container')
+    //gets the size of the grid from the atribute of the container which is passed at the start of the page or when is changed by the sizeBtn 
+    string = container.getAttribute('style')
+    size = parseInt(string.slice(-2))
+    
+    container.innerHTML = ''
+       
+    createGrid(size)
+})
+const sizeBtn = document.querySelector('#size-btn')
+sizeBtn.addEventListener('click', ()=>{
+    let size
+    do {
+        size = prompt('Insert the size of the grid (number max 64)')
+    } while (typeof size == 'number' || size > 64)
     document.querySelector('.container').innerHTML = ''
-    createGrid()
+    createGrid(size)
 })
